cb4a163bf3f8d33e94a30f20e12833aedcc2903f
NicoRB28 <nicolasrodriguezboschi@gmail.com>
2022-11-15 20:20:32 -0300

    un poco mas de la actividad de racionales

--- src/main/scala/week3/RationalDos.scala
+++ src/main/scala/week3/RationalDos.scala
@@ -8,8 +8,16 @@ object RationalDos extends App {
    * utilizando los metodos
    */
   class Rational(x: Int, y: Int) {
-    def numerator   = x
-    def denominator = y
+    require(y != 0, "denominator must not be zero")
+    def this(x: Int) = this(x, 1) //second constructor syntax
+
+    private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+    val numerator   = x / gcd(x, y)
+    val denominator = y / gcd(x, y)
+
+    def less(that: Rational) = numerator * that.denominator < that.numerator * denominator
+
+    def max(that: Rational) = if (this.less(that)) that else this
 
     def add(that: Rational): Rational =
       new Rational(
@@ -22,6 +30,11 @@ object RationalDos extends App {
     def sub(that: Rational): Rational =
       add(that.neg())
 
+    /**
+     * Es mejor la opcion de simplificar en la construccion del racional
+     * porque con valores grandes el toString no funcionara como se espera
+     */
+    //override def toString() = numerator / gcd(x, y) + "/" + denominator / gcd(x, y)
     override def toString() = numerator + "/" + denominator
   }
 
@@ -37,5 +50,15 @@ object RationalDos extends App {
   val rational6 = new Rational(6, 7).neg()
   println("(-5/8) - (-6/7) = " + rational5.sub(rational6))
 
+  val y = new Rational(5, 7)
+  println("5/7 + 5/7 = " + y.add(y))
 
+  val x = new Rational(1, 3)
+
+  println("1/3 < 5/7 = " + x.less(y))
+  println("max(1/3, 5/7) = " + x.max(y))
+
+  //val strange = new Rational(1, 0)
+  val rationalSecondConstructor = new Rational(2)
+  println("rationalSecondConstructor = "+ rationalSecondConstructor)
 }
