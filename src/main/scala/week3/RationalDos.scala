8cbc424c1e442866995d801ec56ff1e1f4f28881
NicoRB28 <nicolasrodriguezboschi@gmail.com>
2022-11-16 17:17:58 -0300

    Identificadores simbolicos

--- src/main/scala/week3/RationalDos.scala
+++ src/main/scala/week3/RationalDos.scala
@@ -15,11 +15,12 @@ object RationalDos extends App {
     val numerator   = x / gcd(x, y)
     val denominator = y / gcd(x, y)
 
-    def less(that: Rational) = numerator * that.denominator < that.numerator * denominator
+    //def less(that: Rational) = numerator * that.denominator < that.numerator * denominator
+    def < (that: Rational) = numerator * that.denominator < that.numerator * denominator
 
-    def max(that: Rational) = if (this.less(that)) that else this
+    def max(that: Rational) = if (this < that) that else this
 
-    def add(that: Rational): Rational =
+    def + (that: Rational): Rational =
       new Rational(
         numerator * that.denominator + that.numerator * denominator,
         denominator * that.denominator
@@ -27,8 +28,8 @@ object RationalDos extends App {
 
     def neg():Rational = new Rational(-numerator, denominator)
 
-    def sub(that: Rational): Rational =
-      add(that.neg())
+    def - (that: Rational): Rational =
+      this + that.neg()
 
     /**
      * Es mejor la opcion de simplificar en la construccion del racional
@@ -40,22 +41,22 @@ object RationalDos extends App {
 
   val rationalOne = new Rational(1, 2)
   val rationalTwo = new Rational(2, 3)
-  println("1/2 + 2/3 = " + rationalOne.add(rationalTwo))
+  println("1/2 + 2/3 = " + (rationalOne + rationalTwo))
 
   val rational3 = new Rational(4, 7)
   val rational4 = new Rational(2, 5)
-  println("4/7 - 2/5 = " + rational3.sub(rational4))
+  println("4/7 - 2/5 = " + (rational3 - rational4))
 
   val rational5 = new Rational(5, 8).neg()
   val rational6 = new Rational(6, 7).neg()
-  println("(-5/8) - (-6/7) = " + rational5.sub(rational6))
+  println("(-5/8) - (-6/7) = " + (rational5 - rational6))
 
   val y = new Rational(5, 7)
-  println("5/7 + 5/7 = " + y.add(y))
+  println("5/7 + 5/7 = " + (y + y))
 
   val x = new Rational(1, 3)
 
-  println("1/3 < 5/7 = " + x.less(y))
+  println("1/3 < 5/7 = " + (x - y))
   println("max(1/3, 5/7) = " + x.max(y))
 
   //val strange = new Rational(1, 0)
